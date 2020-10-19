3397f4d8643a6f41e5063ddfcc3cd8c332fd902b
Victoria Greenfield <victoriagfield@gmail.com>
2020-10-19 15:13:03 -0400

    api routes added, index.js model added

--- develop/routes/apiRoutes.js
+++ develop/routes/apiRoutes.js
@@ -14,7 +14,8 @@ router.post("/api/workouts", (req, res) => {
 router.put("/api/workouts/:id", ({ body, params }, res) => {
   Workout.findByIdAndUpdate(
     params.id,
-    {$push: {exercises: body}},
+    { $push: { exercises: body }},
+    { new: true }
   )
   .then(data =>{
     res.json(data);
@@ -24,8 +25,9 @@ router.put("/api/workouts/:id", ({ body, params }, res) => {
   });
 });
 
+
 router.get("/api/workouts", (req, res) => {
-  Workout.find({})
+  Workout.find()
   .then(data =>{
     res.json(data);
   })
@@ -35,11 +37,13 @@ router.get("/api/workouts", (req, res) => {
 });
 
 router.get("/api/workouts/range", (req, res) => {
-  Workout.find({}).limit(7);
-});
-
-router.delete("/api/workouts", ({ body }, res) => {
-  Workout.findByIdAndDelete(body.id);
+  Workout.find({}).limit(7)
+  .then(data =>{
+    res.json(data);
+  })
+  .catch(err => {
+    res.status(400).json(err);
+  });
 });
 
 module.exports = router;
