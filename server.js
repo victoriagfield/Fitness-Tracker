a357bca67ddf470ee49b34e7d86670b6b88d707d
Victoria Greenfield <victoriagfield@gmail.com>
2020-10-20 16:01:44 -0400

    deploying

--- server.js
+++ server.js
@@ -15,6 +15,8 @@ app.use(express.static("public"));
 
 mongoose.connect("mongodb://localhost/workout", {
   useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
   useFindAndModify: false
 });
 
